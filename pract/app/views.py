9fceb8d7883d0d56e613269ed6da3b371470ef12
Julian Araneda <julianaraneda97@gmail.com>
2019-09-16 04:20:18 -0300

    Añadi la vista listarPersona a views.py

--- pract/app/views.py
+++ pract/app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+
+def listarPersona(request):
+
+	if request.GET.get('id'):
+
+		idPersona = request.GET.get('id')
+
+		p = Termo.objects.filter(id=idPersona)
+
+
+	else:
+
+		t = Persona.objects.all()
+
+	return render_to_response('persona.html', { 'personas' : t })
