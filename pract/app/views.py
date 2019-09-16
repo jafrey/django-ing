462e64e4402e7f22a1ce44ad96fcba985b3a4f29
Julian Araneda <julianaraneda97@gmail.com>
2019-09-16 05:08:30 -0300

    Agregue listarProducto a app/views.py

--- pract/app/views.py
+++ pract/app/views.py
@@ -18,3 +18,18 @@ def listarPersona(request):
 	  p = Persona.objects.all()
 
 	return render_to_response('persona.html', { 'personas' : p })
+
+def listarProducto(request):
+
+    if request.GET.get('id'):
+
+    	idProducto = request.GET.get('id')
+
+    	p = Producto.objects.filter(id=idProducto)
+
+
+    	else:
+
+    	  p = Producto.objects.all()
+
+    return render_to_response('producto.html', { 'producto' : p })
