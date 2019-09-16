789ff33468371cdccaafcd84871e575a429a8995
Julian Araneda <julianaraneda97@gmail.com>
2019-09-16 04:09:21 -0300

    Agregue Compra a los modelos, y un def __str__ a cada modelo, ya que no los tenian y al hacer cosas con foreign key mostraba los modelos horrible, y tuve que investigar.

--- pract/app/models.py
+++ pract/app/models.py
@@ -8,6 +8,9 @@ class Persona(models.Model):
     telefono = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nombre
+
     class Meta:
         verbose_name = "Persona"
         verbose_name_plural = "Personas"
@@ -17,6 +20,22 @@ class Producto(models.Model):
     apellido = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=100)
 
+    def __str__(self):
+         return self.nombre
+
     class Meta:
         verbose_name = "Producto"
         verbose_name_plural = "Productos"
+
+class Compra(models.Model):
+    Persona = models.ForeignKey(Persona)
+    Producto = models.ForeignKey(Producto)
+    Fecha = models.DateField()
+    Hora = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '%s compro %s' % (self.Persona, self.Producto)
+
+    class Meta:
+        verbose_name = "Compra"
+        verbose_name_plural = "Compras"
